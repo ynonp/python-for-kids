970d5983a5b5fc47cbe65045a6da4288749bdd3f
Yotam Shtossel <sh.yotam@gmail.com>
2020-12-02 00:33:51 +0200

    Text writing FIXMEs

---
+++ 05-text-and-fixmes/fixed1.py
@@ -0,0 +1,35 @@
+from p5 import *
+
+# הגדרת משתנים
+myFont = create_font("C:\\Windows\\Fonts\\Arial.ttf", 16)
+x = 3
+
+
+# אתחול חד-פעמי
+def setup():
+    size(400, 400)
+
+
+# ציור (מחזורי)
+def draw():
+    global myFont, x
+
+    # הופכים את המחרוזת בשביל ימין לשמאל
+    string = "שלום עולמי!" [::-1]
+
+    # בחירת רקע וצבע
+    background("white")
+    fill("blue")
+
+    # בחירת גופן ומיקום
+    text_font(myFont, 48)
+    text_align("CENTER")
+
+    # כתיבה למסך
+    text(string, (x, 180))
+
+    # תנועה
+    x = x + 2
+
+
+run()
