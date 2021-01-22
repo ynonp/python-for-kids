2c13aa5d9b99e55378b6b615790cd35fe011f094
Yotam Shtossel <sh.yotam@gmail.com>
2021-01-22 17:39:26 +0200

    Lesson 11 - add Pokemon database program

---
+++ 11-containers/pokemon-db.py
@@ -0,0 +1,40 @@
+# ייבוא ספריות חיצוניות
+import sys
+
+# הגדרת קבועים
+
+# הגדרת משתנים
+kids = {'Yarden': 'Charizard', 'Liya': 'Pikachu'}
+
+
+def print_dict(dictionary):
+    print(dictionary)
+
+
+# קבלת קלט מהמשתמש
+while True:
+    print("הכנס את הפקודה בבקשה:")
+    line = sys.stdin.readline().rstrip()
+    if not line:
+        continue
+
+    input = line.split(' ')
+
+    # הדפס את תוכן המילון
+    if input[0][0] == 'l':
+        print_dict(kids)
+
+    # הוסף רשומה חדשה למילון
+    elif input[0] == 'a':
+        kids[input[1]] = input[2]
+        print_dict(kids)
+
+    # מחיקת רשומה מהמילון
+    elif input[0] == 'd':
+        kids.pop(input[1], None)
+        print_dict(kids)
+
+    # הדפס את הפוקימון ששייך לרשומה
+    elif input[0] == 'g':
+        pokemon = kids.get(input[1], None)
+        print(pokemon)
